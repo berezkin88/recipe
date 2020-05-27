7f76ab2c3da77f71ac296fd8945f7f00c9941316
berezkin88 <berezkin05@gmail.com>
2020-05-27 23:04:14 +0300

    initial commit

---
+++ src/main/java/person/birch/recipe/RecipeApplication.java
@@ -0,0 +1,13 @@
+package person.birch.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeApplication.class, args);
+    }
+
+}
