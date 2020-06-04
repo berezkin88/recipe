9e6d1c94aa3a8427adbca377b05a386ae523bb57
Alexander <berezkin05@gmail.com>
2020-06-05 00:04:12 +0300

    add UnitOfMeasure POJO with unidirectional OneToOne relationship

--- src/main/java/person/birch/recipe/domain/Ingredient.java
+++ src/main/java/person/birch/recipe/domain/Ingredient.java
@@ -16,7 +16,8 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    // private UnitOfMeasure uom;
+    @OneToOne
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -51,5 +52,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
