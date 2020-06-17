727eb802b15e007846e6afaf0e3f93b1822ca3f2
Alexander <berezkin05@gmail.com>
2020-06-17 22:57:05 +0300

    adjust unit test for IndexController

--- src/test/java/person/birch/recipe/controllers/IndexControllerTest.java
+++ src/test/java/person/birch/recipe/controllers/IndexControllerTest.java
@@ -2,16 +2,20 @@ package person.birch.recipe.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+import person.birch.recipe.domain.Recipe;
 import person.birch.recipe.services.RecipeService;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 class IndexControllerTest {
 
@@ -22,6 +26,9 @@ class IndexControllerTest {
 
     @Mock
     Model model;
+
+    @Captor
+    ArgumentCaptor<List<Recipe>> captor;
 
     IndexController controller;
 
@@ -34,11 +41,19 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+
+        List<Recipe> recipes = new ArrayList<>();
+        recipes.add(new Recipe());
+        recipes.add(new Recipe());
+
+        when(recipeService.findAll()).thenReturn(recipes);
+
         String actual = controller.getIndexPage(model);
 
-        verify(model, times(1)).addAttribute(anyString(), any());
+        verify(model, times(1)).addAttribute(anyString(), captor.capture());
         verify(recipeService, times(1)).findAll();
 
         assertEquals(INDEX, actual);
+        assertEquals(2, captor.getValue().size());
     }
 }
