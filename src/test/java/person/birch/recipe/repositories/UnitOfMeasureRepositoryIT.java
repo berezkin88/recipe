3767368cd8c9b77766397b77d34c382b785eb084
Alexander <berezkin05@gmail.com>
2020-06-17 23:27:36 +0300

    add Failsafe Maven plugin and rename integration test

--- src/test/java/person/birch/recipe/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/person/birch/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-class UnitOfMeasureRepositoryTest {
+class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository repository;
