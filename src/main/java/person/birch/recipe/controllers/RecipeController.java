5b561beb362f6b0c77a31c16a86c26ebe6edde5b
Alexander <berezkin05@gmail.com>
2020-06-29 15:43:16 +0300

    add create recipe functionality

--- src/main/java/person/birch/recipe/controllers/RecipeController.java
+++ src/main/java/person/birch/recipe/controllers/RecipeController.java
@@ -2,8 +2,11 @@ package person.birch.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import person.birch.recipe.commands.RecipeCommand;
 import person.birch.recipe.services.RecipeService;
 
 /**
@@ -18,10 +21,24 @@ public class RecipeController {
         this.service = service;
     }
 
-    @RequestMapping("/recipe/show/{id}")
+    @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable Long id, Model model) {
         model.addAttribute("recipe", service.findById(id));
 
         return "/recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = service.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
