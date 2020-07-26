5638b5d2410c187cefc3d1c2131005ca645aa98d
Alexander <berezkin05@gmail.com>
2020-07-26 23:59:16 +0300

    add 404error view, adjust test

--- src/main/java/person/birch/recipe/controllers/RecipeController.java
+++ src/main/java/person/birch/recipe/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package person.birch.recipe.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import person.birch.recipe.commands.RecipeCommand;
+import person.birch.recipe.exceptions.NotFoundException;
 import person.birch.recipe.services.RecipeService;
 
 /**
@@ -53,5 +56,18 @@ public class RecipeController {
 
         service.deleteById(id);
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
